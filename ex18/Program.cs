309243c1d680e21fef091bebcba98e76fec1cfcb
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-08 15:24:49 +0500

    end

--- ex18/Program.cs
+++ ex18/Program.cs
@@ -10,7 +10,7 @@ namespace ex18
     {
         static void Main(string[] args)
         {
-            int[,] array =
+            int[,] twoDimensionalArray =
             {
                 {2, 9, 8, 12},
                 {3, 1, 4, 5}, //=13
@@ -19,9 +19,26 @@ namespace ex18
             };
             int secondLine = 1;
             int firstColumn = 0;
-            int sum = array[secondLine, 0] + array[secondLine, 1] + array[secondLine, 2] + array[secondLine, 3];
-            int multiplication = array[0, 0] * array[1, 0] * array[2, 0];
-            Console.WriteLine($"Сумма второй строки равна: {sum}\nПроизведение первого столбца равно: {multiplication}");
+            int sumOfSecondLine = 0;
+            int multiplicationOfFirstColumn = 1;
+
+            for (int line = 0; line < twoDimensionalArray.GetLength(0); line++)
+            {
+                for (int column = 0; column < twoDimensionalArray.GetLength(1); column++)
+                {
+                    if (column == firstColumn)
+                    {
+                        multiplicationOfFirstColumn *= twoDimensionalArray[line, column];
+                    }
+
+                    if (line == secondLine)
+                    {
+                        sumOfSecondLine += twoDimensionalArray[line, column];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Сумма второй строки равна: {sumOfSecondLine}\nПроизведение первого столбца равно: {multiplicationOfFirstColumn}");
         }
     }
 }
