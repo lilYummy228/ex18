cd637fdbaa6ad93c12c52299ade72d1f8c7ae151
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-08 15:09:34 +0500

    1

--- ex18/Program.cs
+++ ex18/Program.cs
@@ -10,7 +10,18 @@ namespace ex18
     {
         static void Main(string[] args)
         {
-
+            int[,] array =
+            {
+                {2, 9, 8, 12},
+                {3, 1, 4, 5}, //=13
+                {7, 10, 6, 11 }
+              //=42
+            };
+            int secondLine = 1;
+            int firstColumn = 0;
+            int sum = array[secondLine, 0] + array[secondLine, 1] + array[secondLine, 2] + array[secondLine, 3];
+            int multiplication = array[0, 0] * array[1, 0] * array[2, 0];
+            Console.WriteLine($"Сумма второй строки равна: {sum}\nПроизведение первого столбца равно: {multiplication}");
         }
     }
 }
